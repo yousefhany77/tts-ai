faaeaccea670a227f6692e672d116da4e502966d
yousefhany77 <yousefhany2014@gmail.com>
2024-02-01 15:25:00 +0200

    Init commit

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'json-summary', 'html'],
+      thresholds: {
+        lines: 80,
+        branches: 80,
+        functions: 80,
+        statements: 80,
+      },
+    },
+  },
+});
